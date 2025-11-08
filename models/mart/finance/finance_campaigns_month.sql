172a17f43242a722c3e8a01d7fbd2a0a5f458861
elif.cubukcu18@gmail.com <elif.cubukcu18@gmail.com>
2025-11-08 19:18:43 +0000

    edit - campaingns kpi by month

--- models/mart/finance/finance_campaigns_month.sql
+++ models/mart/finance/finance_campaigns_month.sql
@@ -1,15 +1,17 @@
 {{config(materialized = 'table')}}
 select
- extract(month from date_date) as datemonth,
- ads_margin,
- avg_basket,
- avg_operational_margin,
- ads_cost,
- impression,
- click,
- total_products_sold,
- sum_revenue,
- total_purchase_cost,
- total_shipping_fee,
- total_log_cost
-from {{ref("finance_campaigns_day")}}
+ date_trunc(date_date, MONTH) as datemonth,
+ sum(avg_operational_margin - ads_cost) as ads_margin,
+ sum(avg_basket) as avg_basket,
+ sum(avg_operational_margin) as operational_margin,
+ sum(ads_cost) as ads_cost,
+ sum(impression) as ads_impression,
+ sum(click) as ads_click,
+ sum(total_products_sold) as total_products_sold,
+ sum(sum_revenue) as revenue,
+ sum(total_purchase_cost) total_purchase_cost,
+ sum(total_shipping_fee) as total_shipping_fee,
+ sum(total_log_cost) as total_log_cost
+from {{ref("finance_campaigns_day")}} 
+group by datemonth
+order by datemonth desc
